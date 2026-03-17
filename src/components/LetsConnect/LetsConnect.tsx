fb19ab38b219fb4c4c1f748967908c0fd824c1bc
subhashdevkar <subhashsdevkar@gmail.com>
2026-03-17 22:34:53 +0530

    contact form design

--- src/components/LetsConnect/LetsConnect.tsx
+++ src/components/LetsConnect/LetsConnect.tsx
@@ -1,8 +1,24 @@
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { Textarea } from "../ui/textarea"
 
 const LetsConnect = () => {
     return (
-        <div>
-            <div>form</div>
+        <div className="py-[60px] px-20 flex flex-col md:gap-24 md:flex-row md:justify-between">
+            <div>
+                <form action="" className="space-y-5">
+                    <Input className="py-6 px-6 border-black border-2" placeholder="Your Name" />
+                    <Input className="py-6 px-6 border-black border-2" placeholder="Email" />
+                    <Input className="py-6 px-6 border-black border-2" placeholder="Your Website (if exists)" />
+                    <Textarea className="py-6 px-6 border-black border-2" placeholder="How can i help you?" />
+                </form>
+                <div>
+                    <Button>Get In Touch</Button>
+                    <Button>
+
+                    </Button>
+                </div>
+            </div>
             <div className="space-y-10">
                 <div className="space-y-5">
                     <div className="text-5xl font-extrabold">
